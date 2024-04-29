aa44f0e0bffc15ad95094ac9b54cbe97409e84de
DjoykeAbyah <crudendjoyke@gmail.com>
2024-04-29 19:15:50 +0200

    update brackets

--- EXAM_04/brackets/html_brackets/html_brackets.c
+++ EXAM_04/brackets/html_brackets/html_brackets.c
@@ -6,7 +6,7 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/04/17 14:55:29 by dreijans      #+#    #+#                 */
-/*   Updated: 2024/04/18 19:02:12 by dreijans      ########   odam.nl         */
+/*   Updated: 2024/04/29 19:13:23 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,191 +14,127 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-// int ft_strlen(char *str)
-// {
-// 	int i;
+int	ft_strlen(char *str)
+{
+	int i;
 
-// 	i = 0;
-// 	while (str[i] != '\0')
-// 		i++;
-// 	return (i);
-// }
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
 
-// char **alloc_array(int count)
-// {
-// 	char **new_array;
-// 	int i;
+void	ft_strcpy(char *dst, const char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+}
+
+bool tagvalidator(char *str1) 
+{
+    char stack[100];
+	char tag[100];
+	char end[100];
+    int top = -1;
+    int i = 0;
 	
-// 	i = 0;
-// 	new_array = malloc((sizeof(char *)) * (count + 1));
-// 	while (i <= count)
-// 	{
-// 		new_array[i] = NULL;
-// 		i++;
-// 	}
-// 	return (new_array);
-// }
-
-// int ft_strncmp(char *str1, char *str2, int len)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while ((str1[i] != '\0' || str2[i] != '\0') && len > i)
-// 	{
-// 		if (str1[i] != str2[i])
-// 			return (1);
-// 		i++;
-// 	}
-// 	return (0);
-// }
-
-// int find_size(char *str)
-// {
-// 	int i;
-// 	int j;
-
-// 	i = 0;
-// 	j = 0;
-// 	while (str[i] != '\0')
-// 	{
-// 		if (str[i] == '>')
-// 			j++;
-// 		i++;
-// 	}
-// 	return (j);
-// }
-
-// int find_bracket(char *str)
-// {
-// 	int i;
-
-// 	i = 0;
-// 	while (str[i] != '>')
-// 		i++;
-// 	return (i);
-// }
-
-// char **fill_array(char *str, char **new_array)
-// {
-// 	int i;
-// 	int j;
-// 	int result;
+    if (!str1)
+	{
+		write(1, "no string\n", 11);
+		return (false);
+	}
+	if (str1[0] != '<')
+	{
+		return (true);
+	}
+    while (str1[i] != '\0') 
+	{
+        if (str1[i] == '<' && str1[i + 1] != '/') 
+		{
+            int j = i + 1;
+            while (str1[j] != '\0' && str1[j] != '>') 
+			{
+                j++;
+            }
+            if (str1[j] == '>') 
+			{
+                strncpy(tag, &str1[i], j - i + 1);
+                tag[j - i + 1] = '\0';
+                if (tag[ft_strlen(tag) - 2] != '/') 
+				{
+					top++;
+                    ft_strcpy(&stack[top], tag);
+					printf("top = %d\n", top);
+					printf("top = %s\n", &stack[top]);
+                }
+                i = j;
+            }
+        } 
+		else if (str1[i] == '<' && str1[i + 1] == '/') 
+		{
+            int j = i + 2;
+            while (str1[j] != '\0' && str1[j] != '>') 
+			{
+                j++;
+            }
+            if (str1[j] == '>') 
+			{
+                strncpy(end, str1 + i, j - i + 1);
+                end[j - i + 1] = '\0';
+				int k = 0;
+				printf("k = %d\n", k);
+				printf("end = %s\n", end);
+				printf("compare top = %s\n", &stack[top]);
+                if (top == -1 || strcmp(&stack[top + 1], &end[k + 2]) != 0) 
+				{
+					return (false);
+                }
+                top--;
+                i = j;
+            }
+        }
+        i++;
+    }
+    return top == -1;
 	
-// 	i = 0;
-// 	j = 0;
-// 	result = 0;
-// 	while (str[i] != '\0')
-// 	{
-// 		if (str[i] == '<') 
-// 		{
-// 			result = find_bracket(&str[i]) + 1;
-// 			new_array[j] = malloc(sizeof(char) * (result + 1));
-// 			strncpy(new_array[j], &str[i], result);
-// 			new_array[j][result] = '\0';
-// 			i += result;
-// 			j++;
-// 		}
-// 		else
-// 			i++;
-// 	}
-// 	return (new_array);
-// }
+}
 
-// int count_array(char **array)
-// {
-// 	int i;
+// <html></html>
+// <html><body><img/><div></div></body></html>
+// <bla></html><bla></html>
+// <html><bla></html><html></bla></html>
+// <html><html></html><img></html><html><img></html>
+int main(int argc, char **argv) 
+{
+	int i;
 
-// 	i = 0;
-// 	while (array != NULL)
-// 	{
-// 		if (*array == NULL)
-// 		{
-// 			return (i);
-// 		}
-// 		i++;
-// 	}
-// 	return (i);
-// }
-
-// void print_array(char **array)
-// {
-// 	while (array != NULL)
-// 	{
-// 		if (*array == NULL)
-// 		{
-// 			printf("array = NULL\n");
-// 			break ;
-// 		}	
-// 		printf("array = [%s]\n", *array);
-// 		array++;
-// 	}
-// }
-
-// int main (int argc, char **argv)
-// {
-// 	int i;
-// 	int j;
-// 	int k;
-// 	int iterator;
-// 	int size_open;
-// 	int size_close;
-// 	char **array;
-// 	char **open;
-// 	char **close;
-
-// 	i = 1;
-// 	j = 0;
-// 	k = 0;
-// 	iterator = 0;
-// 	array = NULL;
-// 	open = NULL;
-// 	close = NULL;
-// 	size_open = 0;
-// 	if (argc == 1)
-// 	{
-// 		write(1, "no input\n", 10);
-// 		return (0);
-// 	}
-// 	array = alloc_array(find_size(argv[i]));
-// 	array = fill_array(argv[i], array);
-// 	open = alloc_array(argc);
-// 	close = alloc_array(argc);
-// 	while (array[iterator] != NULL)
-// 	{
-// 		if (strcmp(array[iterator], "<img>") != 0)
-// 		{
-// 			if (ft_strncmp(array[iterator], "</", 2) == 0)
-// 			{
-// 				close[j] = malloc((sizeof (char)) * (ft_strlen(array[iterator])));
-// 				close[j] = strncpy(close[j], array[iterator], ft_strlen(array[iterator]));
-// 				close[ft_strlen(array[iterator])] = "\0";
-// 				j++;
-// 			}
-// 			else
-// 			{
-// 				open[k] = malloc((sizeof (char)) * (ft_strlen(array[iterator]) + 1));
-// 				open[k] = strncpy(open[k], array[iterator], ft_strlen(array[iterator]));
-// 				open[ft_strlen(array[iterator])] = "\0";
-// 				k++;
-// 			}
-// 			iterator++;
-
-// 		}
-// 		iterator++;	
-// 	}
-// 	size_close = count_array(close);
-// 	size_open = count_array(open);
-// 	printf("int size open = [%i]\n", size_open);
-// 	if (size_close != size_open)
-// 	{
-// 		write(1, "KO\n", 3);
-// 		return (0);
-// 	}
-// 	else
-// 	{
-// 		write(1, "OK\n", 3);
-// 		return (0);
-// 	}
-// }
+	i = 1;
+	while (i < argc)
+	{
+		if (tagvalidator(argv[i]) == false)
+		{
+			write(1, "KO\n", 3);
+			return (1);
+		}
+		else if (tagvalidator(argv[i]) == true)
+		{
+			write(1, "OK\n", 3);
+			return (0);
+		}
+		if (argc == 1)
+		{
+			write(1, "\n", 1);
+			return (1);
+		}
+	}
+    return 0;
+}
