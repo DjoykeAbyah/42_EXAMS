3d74fac92a4fde04667e197b45cb89f5f706f0d2
DjoykeReijans <crudendjoyke@gmail.com>
2024-09-12 12:52:50 +0200

    new

--- practice/practice_html2.c
+++ practice/practice_html2.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 typedef struct s_list {
 	char *value;
@@ -9,41 +9,54 @@ typedef struct s_list {
 } t_list;
 
 t_list *init_node(char *value) {
-	
-	t_list *new_node;
-	new_node = malloc(sizeof (t_list));
+
+	t_list *new_node = malloc(sizeof (t_list));
 	new_node->value = value;
 	new_node->next = NULL;
 	return (new_node);
 }
 
-t_list *list_last(t_list *list) {
-	while (list && list->next)
-		list = list->next;
-	return (list);
+t_list *list_last(t_list *lst) {
+	while (lst && lst->next) //check if lst exists and ther is next
+		lst = lst->next;
+	return (lst);
 }
 
-void ft_list_add_back(t_list **list, t_list *new) {
+void ft_list_add_back(t_list **lst, t_list *new) {
 	t_list *back;
-	if (*list) {
-		back = list_last(*list);
+
+	if (*lst) {
+		back = list_last(*lst);
 		back->next = new;
 	}
 	else
-		*list = new;
+		*lst = new;
 }
 
-int compare_words(t_list **list, char *word) {
+char *extract_tag(char *str, int len) {
+	int i = 0;
+	char *tag = malloc(sizeof (char) * len + 1);
+
+	while (i < len && str[i] != ' ' && str[i] != '/') { //while i < len
+		tag[i] = str[i];
+		i++;
+	}
+	tag[i] = '\0';
+	return (tag);
+}
+
+int compare_words(t_list **stack, char *word) { //double pointer els no erase
 	t_list *previous = NULL;
-	t_list *current = *list;
+	t_list *current = *stack;
 
 	while (current->next) {
 		previous = current;
 		current = current->next;
 	}
-	if (current && strcmp(current->value, word) == 0) {
-		if (previous == NULL)
-			*list = NULL; //only one node in list
+	if (current && strcmp(current->value, word) == 0) { //need to be current
+		if (previous == NULL) {
+			*stack = NULL; //only one node in list
+		}
 		else
 			previous->next = NULL; //erase last in list
 		free(current->value);
@@ -54,38 +67,25 @@ int compare_words(t_list **list, char *word) {
 		return (1);
 }
 
-char *extract_tag_name(char *str, int len) {
-    // This function extracts the tag name from the string, ignoring attributes.
-    char *tag = malloc(len + 1);
-    int i = 0;
-
-    while (i < len && str[i] != ' ' && str[i] != '/') {
-        tag[i] = str[i];
-        i++;
-    }
-    tag[i] = '\0';
-    return tag;
-}
-
-int parse_words(char *str) {
+int validate_html(char *str) {
 	int i = 0;
 	t_list *stack = NULL;
-	
+
 	while (str[i]) {
 		if (str[i] == '<' && str[i + 1] != '/') {
 			int j = i + 1;
 			while (str[i] != '>')
 				i++;
-			if (str[i] ==  '>') {
+			if (str[i] == '>') {
 				int len = i - j;
-				char *word = extract_tag_name(&str[j], len);
-				if (strcmp(word, "img") == 0) {
+				char *word = extract_tag(&str[j], len);
+				if (strcmp(word, "img") == 0)
 					i = j;
-					return 0;
+				else {
+					printf("word = %s\n", word);
+					t_list *new_node = init_node(word);
+					ft_list_add_back(&stack, new_node);
 				}
-				printf("word = %s\n", word);
-				t_list *new_node = init_node(word);
-				ft_list_add_back(&stack, new_node);
 			}
 			i = j;
 		}
@@ -93,9 +93,9 @@ int parse_words(char *str) {
 			int j = i + 2;
 			while (str[i] != '>')
 				i++;
-			if (str[i] ==  '>') {
+			if (str[i] == '>') {
 				int len = i - j;
-				char *word = extract_tag_name(&str[j], len);
+				char *word = extract_tag(&str[j], len);
 				printf("word2 = %s\n", word);
 				if (compare_words(&stack, word) == 1) {
 					free(word);
@@ -108,7 +108,7 @@ int parse_words(char *str) {
 					}
 					return (1);
 				}
-				free(word); 
+				free(word);
 			}
 			i = j;
 		}
@@ -128,15 +128,16 @@ int parse_words(char *str) {
 }
 
 int main(int argc, char **argv) {
+	
 	int i = 1;
 
 	if (argc == 1)
-		return (write(1, "\n", 1));
+		return(write(1, "\n", 1));
 	while (argv[i]) {
-		if (parse_words(argv[i]) == 0)
-			write(1, "OK\n", 3);
+		if (validate_html(argv[i]) == 0)
+			write (1, "OK\n", 3);
 		else
-			write(1, "Error\n", 6);
+			write (1, "Error\n", 6);
 		i++;
 	}
 	return (0);
